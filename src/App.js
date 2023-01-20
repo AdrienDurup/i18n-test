d940852f1073bca6cd14e6485736e3af0b7a93a1
Adrien Durup <adurup@alliancegravity.com>
2023-01-20 16:13:20 +0100

    rebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 
 import Inbox from './Inbox.tsx';
@@ -17,30 +16,4 @@ function App() {
 
 
 
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
->>>>>>> 1e440ff (rebase)
 export default App;
